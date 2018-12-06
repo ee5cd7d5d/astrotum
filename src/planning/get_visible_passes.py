86b2a0eed6f90043ed2f7c21a17e8ef3f8f8eea6
Andrea Meraner <andrea.meraner@tum.de>
2018-12-06 23:29:25 +0100

    working basic reading of webpage table

--- src/planning/get_visible_passes.py
+++ src/planning/get_visible_passes.py
@@ -9,39 +9,43 @@ Created on Sun Dec  2 22:33:22 2018
 import numpy as np
 from bs4 import BeautifulSoup
 from selenium import webdriver
-from lxml import etree
 
 options = webdriver.ChromeOptions()
+options.add_argument('headless') #makes it invisible. Comment to see webpage displayed
+options.add_argument('window-size=1200x600')
+path_to_chrome_driver=r"chromedriver.exe"
 
-class GrowingList(list):
-    def __setitem__(self, index, value):
-        if index >= len(self):
-            self.extend([None]*(index + 1 - len(self)))
-        list.__setitem__(self, index, value)
         
 def read_webpage(day, month, year, max_mag):
     
     url='https://in-the-sky.org/satpasses.php?day='+day+'&month='+month+'&year='+year+'&mag='+max_mag+'&anysat=v0&group=1&s='
-    print(url)
-    options.add_argument('headless')
-    options.add_argument('window-size=1200x600')
-    path_to_chrome_driver=r"C:\Users\andreameraner\dev\chromedriver.exe"
+    print("Getting pass information from: ",url)
+    
+    print("Opening Chrome webdriver")
     browser = webdriver.Chrome(path_to_chrome_driver,options=options)
+    print("Opening webpage")
     browser.get(url)
     
     html = browser.page_source
     soup = BeautifulSoup(html, 'html.parser')
     #print(soup)
+    print("Webpage opened and read, now getting table")
     
-    passes_table = soup.find_all('tbody')[1]
 
+    passes_table = soup.find_all('tbody')[1]
+    #print(passes_table)
+    
     passes_info=list()
     for row_content in passes_table.find_all('tr'):
-        new_row = [None]*15 #initialize None row, robust to missing values
+        new_row = [None]*16 #initialize None row, robust to missing values
         for col_n, col_content in enumerate(row_content.find_all('td')):
             new_row[col_n] = col_content.get_text()
+            if col_n == 0:
+                link = col_content.find('a')
+                new_row[15] = link.get('href')
         passes_info.append(new_row)
-        
+    
+    print("Got pass information table!")
     return np.asarray(passes_info)
     
 
@@ -54,13 +58,13 @@ if __name__ == '__main__':
 
     max_mag = 3
 
-    start_time_hour = 18
-    start_time_min = 00
-    stop_time_hour = 23
-    stop_time_min = 59
+    #start_time_hour = 18
+    #start_time_min = 00
+    #stop_time_hour = 23
+    #stop_time_min = 59
     
     passes_info = read_webpage(str(day), str(month), str(year), str(max_mag))
-    print(passes_info)
+    #print(passes_info)
     
     
     
