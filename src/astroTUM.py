61cb01aa95ba7096dc02e85cd3bf66bcae5be3a2
Pedro Rodriguez Perez <pedrodper@gmail.com>
2018-12-28 11:50:47 +0100

    Add load function. Update header

--- src/astroTUM.py
+++ src/astroTUM.py
@@ -10,13 +10,17 @@ import matplotlib.pyplot as plt
 ### CAREFUL: IT DOES NOT WORK PROPERLY
 ### UNCOMMENTED AND HORRIBLE. 
 
+def load_fits(path : str):
+    '''Load fits file from given path'''
+    return aio.fits.open(path)[0]
+
 def mean_from_folder(path : str, image_base=None) -> np.ndarray:
     fits_list = sorted([x for x in os.listdir(path) if x.lower().endswith('.fits')])
     master_key = fits_list[0]
 
     fits_dict = {}
     for ifits in fits_list:
-        fits_dict[ifits] = aio.fits.open(os.path.join(path, ifits))[0]
+        fits_dict[ifits] = load_fits(os.path.join(path, ifits))
 
     if image_base is not None and \
        fits_dict[master_key].shape != image_base.data.shape:
@@ -48,6 +52,18 @@ def process_root(root_dir : str):
                                        image_base=flat_ccd)
     light_ccd = nd.CCDData(data=light_data_mean, unit="adu")
     return light_ccd
+
+def cut_fits(fits, x_offset, y_offset, width, height):
+    """Returns an updated cut version of the fits file"""
+    if width is None:
+        width = len(fits.data)-x_offset
+    if height is None:
+        height = len(fits.data)-y_offset
+    fits.data = fits.data[x_offset:width, y_offset:height]
+    fits.header['NAXIS1'] = width
+    fits.header['NAXIS2'] = height
+
+    return fits
 
 
 #root_dir = '/home/pedro/Projects/PYTHON/astro/18_10_17/'
